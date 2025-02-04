69c54a7c6b349c2112bd44aea9990d4db7bd08d5
TheMoJd <moetez.jaoued@etudiant.univ-brest.fr>
2025-02-04 11:15:08 +0100

    debugger-with-java

---
+++ src/dgb/JDISimpleDebugger.java
@@ -0,0 +1,11 @@
+package dgb;
+
+public class JDISimpleDebugger {
+    public static void main(String[] args) throws Exception {
+
+        ScriptableDebugger debuggerInstance = new ScriptableDebugger();
+        debuggerInstance.attachTo(JDISimpleDebuggee.class);
+
+    }
+}
+
---
+++ src/dgb/ScriptableDebugger.java
@@ -0,0 +1,74 @@
+package dgb;
+
+import com.sun.jdi.*;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.connect.VMStartException;
+import com.sun.jdi.event.*;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Map;
+
+public class ScriptableDebugger {
+
+    private Class debugClass;
+    private VirtualMachine vm;
+
+    public VirtualMachine connectAndLaunchVM() throws IOException, IllegalConnectorArgumentsException, VMStartException {
+        LaunchingConnector launchingConnector = Bootstrap.virtualMachineManager().defaultConnector();
+        Map<String, Connector.Argument> arguments = launchingConnector.defaultArguments();
+        arguments.get("main").setValue(debugClass.getName());
+        VirtualMachine vm = launchingConnector.launch(arguments);
+        return vm;
+    }
+
+    public void attachTo(Class debuggeeClass) {
+
+        this.debugClass = debuggeeClass;
+        try {
+            vm = connectAndLaunchVM();
+            enableClassPrepareRequest(vm);
+            startDebugger();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalConnectorArgumentsException e) {
+            e.printStackTrace();
+        } catch (VMStartException e) {
+            e.printStackTrace();
+            System.out.println(e.toString());
+        } catch (VMDisconnectedException e) {
+            System.out.println("Virtual Machine is disconnected: " + e.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void enableClassPrepareRequest(VirtualMachine vm) {
+    }
+
+    public void startDebugger() throws VMDisconnectedException, InterruptedException {
+        EventSet eventSet = null;
+        while ((eventSet = vm.eventQueue().remove()) != null) {
+            for (Event event : eventSet) {
+                System.out.println(event.toString());
+                if (event instanceof VMDisconnectEvent) {
+                    System.out.println("End of program");
+                    InputStreamReader reader = new InputStreamReader(vm.process().getInputStream());
+                    OutputStreamWriter writer = new OutputStreamWriter(System.out);
+                    try {
+                        reader.transferTo(writer);
+                        writer.flush();
+                    } catch (IOException e) {
+                        System.out.println("Target VM input stream reading error.");
+                    }
+                }
+                vm.resume();
+            }
+        }
+    }
+
+}
